370dca8800ace6a38cbbeb872c43ed9469fc3c0f
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-11-23 10:30:15 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,56 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('message', message => {
+  if (!message.guild) return;
+  if (message.content === 'تعال') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('kk <3');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('هخ , متخش الروم يعلء');
+    }
+  }
+});
+
+const devs = ["280749272498962432"];
+const adminprefix = ["-"];
+Client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    Client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  Client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  Client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    Client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  Client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  Client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+client.login("process.env.BOT_TOKEN");
